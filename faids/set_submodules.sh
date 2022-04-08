1a8ebe8116380d9091b8bcb147e53bad9b2f7f28
Farzin Firouzi <farzineff@gmail.com>
2022-04-07 20:35:34 -0400

    --

--- faids/set_submodules.sh
+++
@@ -1,3 +0,0 @@
-git submodule add https://github.com/tpope/vim-commentary.git .config/nvim/plugged/vim-commentary
-git submodule add https://github.com/terryma/vim-expand-region.git .config/nvim/plugged/vim-expand-region
-git submodule add https://github.com/tpope/vim-surround.git .config/nvim/plugged/vim-surround
