0743bf7ef8430aa3110061390a0aa2f027900df0
Farzin Firouzi <farzineff@gmail.com>
2024-08-17 18:41:21 -0700

    key bind

--- init.el
+++ init.el
@@ -444,7 +444,7 @@
     (define-key evil-normal-state-map (kbd "ZQ") 'fff-revert-and-bury-buffer)
     (define-key evil-normal-state-map (kbd "o") 'fff-evil-open-below)
     (define-key evil-normal-state-map (kbd "O") 'fff-evil-open-above)
-    (define-key evil-normal-state-map (kbd "C-/") 'fff-comment)
+    (define-key evil-normal-state-map (kbd "C-/") 'fff-comment)))
 
     (load (expand-file-name "fff-functions.el" user-emacs-directory))))
 
