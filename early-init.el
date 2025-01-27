37513437069923afbcf9b54fc5cb806d8072a833
ff <>
2025-01-27 15:58:25 -0800

    garbage collection.

--- early-init.el
+++ early-init.el
@@ -1,5 +1,17 @@
-(setq package-enable-at-startup nil)
-(setq gc-cons-threshold most-positive-fixnum)
+;; garbage collection tweak
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.5)
+(defvar fff-emacs--file-name-handler-alist file-name-handler-alist)
+(defvar fff-emacs--vc-handled-backends vc-handled-backends)
+(setq file-name-handler-alist nil
+      vc-handled-backends nil)
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold (* 1000 1000 8)
+                  gc-cons-percentage 0.1
+                  file-name-handler-alist fff-emacs--file-name-handler-alist
+                  vc-handled-backends fff-emacs--vc-handled-backends)))
+
 (setq package-enable-at-startup nil)
 (setq load-prefer-newer noninteractive)
 (set-language-environment "UTF-8")
