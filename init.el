b4f041d7407d7dc44905abd44ccdbb0242c64ef3
Farzin Firouzi <farzineff@gmail.com>
2025-08-18 11:41:51 -0700

    formatting

--- init.el
+++ init.el
@@ -245,7 +245,7 @@
   (global-set-key (kbd "M-ESC M-ESC") 'keyboard-escape-quit)           ;; I have to remap to this instead
   (global-set-key (kbd "M-u") 'universal-argument)                     ;; C-u is bound to evil-scroll-up
   (global-set-key (kbd "C-x k") 'bury-buffer)                          ;; kill buffers doesn't save memory
-  (global-set-key (kbd "C-c c")  'fff-clear-shell)
+  (global-set-key (kbd "C-c c") 'fff-clear-shell)
   (global-set-key (kbd "C-g")  #'fff-keyboard-quit-dwim)
   (global-set-key [remap find-file] 'fff-find-file)                    ;; updates the current directory when in vterm
   (global-set-key [remap list-buffers] 'ibuffer)                       ;; ibuffer is superior
